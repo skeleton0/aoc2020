4617bce849e15d1c9316cdd8f929b80bb645aa07
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2020-12-03 17:14:05 +1100

    Improve solution

--- day3/part2.py
+++ day3/part2.py
@@ -3,7 +3,7 @@ from aocd import get_data, submit
 data = get_data(year=2020, day=3)
 
 paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-answer = 0
+answer = 1
 for x_incr, y_incr in paths:
     trees = 0
     x = x_incr
@@ -13,9 +13,6 @@ for x_incr, y_incr in paths:
         x += x_incr
         if x >= len(line):
             x = x - len(line)
-    if answer == 0:
-        answer = trees
-    else:
-        answer *= trees
+    answer *= trees
 
 submit(answer, part='b', year=2020, day=3)
